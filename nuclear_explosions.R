fdf9a36bc85f747689c1014a96a2774124d1678c
Malte Schmielewski <malte.schmielewski@goal-analytics.com>
2019-08-22 18:16:13 +0200

    nearly done

--- nuclear_explosions.R
+++ nuclear_explosions.R
@@ -1,14 +1,20 @@
-nuclear_explosions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-20/nuclear_explosions.csv")
-
-nuclear_explosions=as.data.table(nuclear_explosions)
-#install.packages(c("maps", "mapdata"))
-#evtools::install_github("dkahle/ggmap")
-
+#nuclear_explosions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-20/nuclear_explosions.csv")
+#write.csv(nuclear_explosions,"nuclear_explosions.csv")
 library(ggplot2)
 library(ggmap)
 library(maps)
 library(mapdata)
 library(png)
+library(grid)
+
+
+
+nuclear_explosions <- readr::read_csv("nuclear_explosions.csv")
+
+
+
+nuclear_explosions=as.data.table(nuclear_explosions)
+
 
 world_map <- map_data("world")
 p<-ggplot(world_map, aes(x = long, y = lat, group = group)) +
@@ -27,14 +33,15 @@ for(i in 1:nrow(nuclear_explosions)){
 
 p + theme(
   panel.background = element_rect(fill = "white"),
-  plot.margin = margin(2, 2, 2, 2, "cm"),
+  plot.margin = margin(1, 1, 1, 1, "cm"),
   plot.background = element_rect(
     fill = "white",
     colour = "black",
     size = 1
   )
-)  +annotate("text", x=10, y=10, label= "There has been 2051 nuclear explosions in the last 74 years all around the globe") + 
-  annotate("text", x=8, y=1, label= "TidyTuesday - Twitter: @schmalte04")
+)  
++annotate("text", x=10, y=20, label= "There has been 2051 nuclear explosions in the last 74 years all around the globe") + 
+annotate("text", x=8, y=1, label= "TidyTuesday - Twitter: @schmalte04")
 #grid.text("There has been 2051 nuclear explosions in the last 74 years all around the globe", x = unit(0.5, "npc"), y = unit(0.17, "npc"))
 #grid.text("TidyTuesday - Twitter: @schmalte04", x = unit(0.5, "npc"), y = unit(0.17, "npc"))
 
