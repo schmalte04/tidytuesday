fd1845c9737846a0b2ec83eea0e93b1c44a63241
Malte Schmielewski <malte.schmielewski@goal-analytics.com>
2019-09-16 22:51:05 +0200

    added new data week

---
+++ nationalparks/nationalparks.R
@@ -0,0 +1,4 @@
+park_visits <- read.csv("~/workspace/tidytuesday/nationalparks/park_visits.csv", row.names=1)
+gas_price <- read.csv("~/workspace/tidytuesday/nationalparks/gas_price.csv", row.names=1)
+state_pop<- read.csv("~/workspace/tidytuesday/nationalparks/state_pop.csv", row.names=1)
+
